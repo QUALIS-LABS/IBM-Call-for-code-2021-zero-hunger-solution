19d5851f74ebe7fa8b4a1f8ed07f4b948ba4780a
smithjilks <smithjilks@gmail.com>
2021-06-19 00:09:37 +0300

    Add login activity layout

--- Android/src/app/src/main/java/com/qualislabs/mashinani/LoginActivity.java
+++ Android/src/app/src/main/java/com/qualislabs/mashinani/LoginActivity.java
@@ -1,18 +1,14 @@
 package com.qualislabs.mashinani;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import life.sabujak.roundedbutton.RoundedButton;
-
-public class IntroActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_login);
+
     }
 }
