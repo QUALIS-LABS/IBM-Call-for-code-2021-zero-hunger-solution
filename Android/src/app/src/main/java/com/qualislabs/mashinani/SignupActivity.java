265535bb8c912b85535af01a2c0cbd01c78fa4c1
smithjilks <smithjilks@gmail.com>
2021-06-19 00:38:15 +0300

    Add signup activity layout

--- Android/src/app/src/main/java/com/qualislabs/mashinani/SignupActivity.java
+++ Android/src/app/src/main/java/com/qualislabs/mashinani/SignupActivity.java
@@ -2,18 +2,13 @@ package com.qualislabs.mashinani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import life.sabujak.roundedbutton.RoundedButton;
-
-public class IntroActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
-
+        setContentView(R.layout.activity_signup);
     }
 }
