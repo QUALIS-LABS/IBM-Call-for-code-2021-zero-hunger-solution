9acd70c5cb7de60fdf9c78466016a51a7f445762
smithjilks <smithjilks@gmail.com>
2021-06-26 19:10:02 +0300

    Add trader_requisition activity layout

---
+++ Android/src/app/src/main/java/com/qualislabs/mashinani/TraderRequisitionActivity.java
@@ -0,0 +1,14 @@
+package com.qualislabs.mashinani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TraderRequisitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trader_requisition);
+    }
+}
