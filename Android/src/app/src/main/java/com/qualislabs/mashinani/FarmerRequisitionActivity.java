597687c8d49b8779a4760488663cafcd1380939e
smithjilks <smithjilks@gmail.com>
2021-06-26 17:59:13 +0300

    Add farmer_requisition activity layout

---
+++ Android/src/app/src/main/java/com/qualislabs/mashinani/FarmerRequisitionActivity.java
@@ -0,0 +1,14 @@
+package com.qualislabs.mashinani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmerRequisitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_requisition);
+    }
+}
