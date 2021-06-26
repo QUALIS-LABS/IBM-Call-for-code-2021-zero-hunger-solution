081e81fce0140ef697b042a8b6a7dd0f9a5f1c71
smithjilks <smithjilks@gmail.com>
2021-06-26 16:46:56 +0300

    Add trader_dispatch_tracking activity layout

---
+++ Android/src/app/src/main/java/com/qualislabs/mashinani/TraderDispatchTrackingActivity.java
@@ -0,0 +1,14 @@
+package com.qualislabs.mashinani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TraderDispatchTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trader_dispatch_tracking);
+    }
+}
