dea54f0e1dd4b697a56943398dd7e99a50feba97
smithjilks <smithjilks@gmail.com>
2021-06-27 17:30:51 +0300

    Create login component

--- Dashboard/src/src/app/app.module.ts
+++ Dashboard/src/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from "./auth/login/login.component";
+
+import { MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
