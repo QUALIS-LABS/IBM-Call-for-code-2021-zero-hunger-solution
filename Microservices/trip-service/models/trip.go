2d6d44c47205a18c448797cc4dacf7abe2cdca07
sammy <sammyoina@gmail.com>
2021-07-29 22:23:34 +0300

    Update trip.go

--- Microservices/trip-service/models/trip.go
+++ Microservices/trip-service/models/trip.go
@@ -21,9 +21,9 @@ type Trip struct {
 
 //CreateTripInput fields
 type CreateTripInput struct {
-	DriverID            int    `json:"driverId"`
-	TripName            string `json:"tripName"`
-	TraderRequisitionID uint   `json:"traderRequisitionID"`
-	FarmerRequisitionID uint   `json:"farmerRequisitionId`
-	Status              string `json:"status"`
+	DriverID            int            `json:"driverId"`
+	TripName            string         `json:"tripName"`
+	TraderRequisitionID uint           `json:"traderRequisitionID"`
+	FarmerRequisitionID pq.StringArray `json:"farmerRequisitionId gorm:"type:text[]"`
+	Status              string         `json:"status"`
 }
